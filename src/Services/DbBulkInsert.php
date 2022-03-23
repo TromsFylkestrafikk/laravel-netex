3e4edadd9c6506b4c014c726997ba170cf064158
Kåre Slettnes <kaare@slettnes.org>
2022-03-23 13:18:48 +0100

    Issue #9: Allow several methods in DbBulkInsert
    Also commented and chain-ified class

--- src/Services/DbBulkInsert.php
+++ src/Services/DbBulkInsert.php
@@ -2,9 +2,20 @@
 
 namespace TromsFylkestrafikk\Netex\Services;
 
+use InvalidArgumentException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
+/**
+ * Add DB records in bulk.
+ *
+ * In certain scenarios creating DB connections are costly, and frequent
+ * DB::insert (e.g. in loops) may choke the performance.
+ *
+ * This tool can be used to buffer up db records connected to tables and write
+ * them in bulk when buffer is full. The buffer is also flushed/written on
+ * destruct, but adding a final $this->flush() is preferred.
+ */
 class DbBulkInsert
 {
     /**
@@ -36,6 +47,14 @@ class DbBulkInsert
 
     public function __construct(string $table, $method = 'insert')
     {
+        $allowedMethods = ['insert', 'insertOrIgnore'];
+        if (!in_array($method, $allowedMethods)) {
+            throw new InvalidArgumentException(sprintf(
+                '%s: $method must be one of: %s',
+                self::class,
+                implode(', ', $allowedMethods)
+            ));
+        }
         $this->table = $table;
         $this->method = $method;
         $this->resetBuffer();
@@ -46,6 +65,11 @@ class DbBulkInsert
         $this->flush();
     }
 
+    /**
+     * @param array $record
+     *
+     * @return $this
+     */
     public function addRecord($record)
     {
         $this->records[] = $record;
@@ -53,12 +77,19 @@ class DbBulkInsert
         if ($this->recordCount >= $this->bufferSize) {
             $this->flush();
         }
+        return $this;
     }
 
+    /**
+     * Write buffered records to DB.
+     *
+     * @return $this
+     */
     public function flush()
     {
-        DB::table($this->table)->insert($this->records);
+        DB::table($this->table)->{$this->method}($this->records);
         $this->resetBuffer();
+        return $this;
     }
 
     protected function resetBuffer()
