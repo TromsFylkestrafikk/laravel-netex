881522499c3982fdb3f7726665ed07a42f2b73da
tft-kyrre <kyrre.andr.hansen@tffk.no>
2023-03-03 11:55:21 +0100

    Issue #21: DocBlock added to ImportStatus model.

--- src/Models/ImportStatus.php
+++ src/Models/ImportStatus.php
@@ -4,6 +4,17 @@ namespace TromsFylkestrafikk\Netex\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * TromsFylkestrafikk\Netex\Models\ImportStatus
+ *
+ * @property string $name
+ * @property int $size
+ * @property string $md5
+ * @property string|null $import_date
+ * @property string|null $valid_to
+ * @property int $status
+ * @property int $version
+ */
 class ImportStatus extends Model
 {
     protected $table = 'netex_import';
