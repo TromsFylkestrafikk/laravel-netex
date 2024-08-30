6a4b913395e896b11ea66e0bf48f25d709078190
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2024-08-30 13:54:04 +0200

    Issue #29: Re-factored the compex calendar build mechanism.
    This needs a follow-up.

--- src/Services/RouteImporter.php
+++ src/Services/RouteImporter.php
@@ -2,6 +2,7 @@
 
 namespace TromsFylkestrafikk\Netex\Services;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use SimpleXMLElement;
@@ -12,7 +13,7 @@ use TromsFylkestrafikk\Xml\ChristmasTreeParser;
  */
 class RouteImporter
 {
-    public $description = '';
+    public $description = null;
     public $availableFrom = null;
     public $availableTo = null;
     public $version = null;
@@ -23,9 +24,11 @@ class RouteImporter
     protected $dumpers = [];
 
     /**
-     * List of namespaces in currently parsed XML
+     * Various storage containers during parsing of calendar dates.
      */
-    protected $namespaces = [];
+    protected $dayTypes = [];
+    protected $operatingPeriods = [];
+    protected $dayTypeAssignments = [];
 
     public function __construct(protected RouteSet $set)
     {
@@ -54,13 +57,21 @@ class RouteImporter
      */
     public function importSharedData(string $xmlFile): RouteImporter
     {
+        $this->dayTypes = [];
+        $this->operatingPeriods = [];
+        $this->dayTypeAssignments = [];
+
         $elementToTable = [
-            'Calendar' => 'netex_calendar',
             'Operator' => 'netex_operators',
             'GroupOfLines' => 'netex_line_groups',
             'DestinationDisplay' => 'netex_destination_displays',
             'ScheduledStopPoint' => 'netex_stop_points',
             'ServiceLink' => 'netex_service_links',
+            'PassengerStopAssignment' => 'netex_stop_assignments',
+            'Notice' => 'netex_notices',
+            'Block' => 'netex_vehicle_blocks',
+            'BlockJourneyRef' => 'netex_vehicle_schedules',
+            'Calendar' => 'netex_calendar',
         ];
         foreach ($elementToTable as $element => $table) {
             DB::table($table)->truncate();
@@ -87,6 +98,12 @@ class RouteImporter
                     ['ServiceFrame', 'destinationDisplays', 'DestinationDisplay'],
                     ['ServiceFrame', 'scheduledStopPoints', 'ScheduledStopPoint'],
                     ['ServiceFrame', 'serviceLinks', 'ServiceLink'],
+                    ['ServiceFrame', 'stopAssignments', 'PassengerStopAssignment'],
+                    ['ServiceFrame', 'notices', 'Notice'],
+                    ['VehicleScheduleFrame', 'blocks', 'Block'],
+                    ['ServiceCalendarFrame', 'dayTypes', 'DayType'],
+                    ['ServiceCalendarFrame', 'operatingPeriods', 'OperatingPeriod'],
+                    ['ServiceCalendarFrame', 'dayTypeAssignments', 'DayTypeAssignment'],
                 ];
                 foreach ($underFrames as $children) {
                     $leafElement = end($children);
@@ -104,6 +121,7 @@ class RouteImporter
             /** @var DbBulkInsert $dumper */
             $dumper->flush();
         }
+        $this->writeCalendar();
         return $this;
     }
 
@@ -164,10 +182,140 @@ class RouteImporter
         ]);
     }
 
+    protected function readPassengerStopAssignment(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $this->dumpers['PassengerStopAssignment']->addRecord([
+            'id' => $xml['id'],
+            'order' => $xml['order'],
+            'stop_point_ref' => $xml->ScheduledStopPointRef['ref'],
+            'quay_ref' => $xml->QuayRef['ref'],
+        ]);
+    }
+
+    protected function readNotice(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $this->dumpers['Notice']->addRecord([
+            'id' => $xml['id'],
+            'text' => $xml->Text,
+            'public_code' => $xml->PublicCode,
+        ]);
+    }
+
+    protected function readBlock(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $blockId = $xml['id'];
+        $this->dumpers['Block']->addRecord([
+            'id' => $blockId,
+            'private_code' => $xml->PrivateCode,
+            'calendar_ref' => $xml->dayTypes->DayTypeRef['ref'],
+        ]);
+        foreach ($xml->journeys->VehicleJourneyRef as $journeyRef) {
+            $this->dumpers['BlockJourneyRef']->addRecord([
+                'vehicle_block_ref' => $blockId,
+                'vehicle_journey_ref' => $journeyRef['ref'],
+            ]);
+        }
+    }
+
+    protected function readDayType(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $id = (string) $xml['id'];
+        $this->dayTypes[$id]['DaysOfWeek'] = isset($xml->properties)
+           ? ((string) $xml->properties->PropertyOfDay->DaysOfWeek)
+           : null;
+    }
+
+    protected function readOperatingPeriod(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $id = (string) $xml['id'];
+        $this->operatingPeriods[$id]['FromDate'] = $xml->FromDate;
+        $this->operatingPeriods[$id]['ToDate'] = $xml->ToDate;
+    }
+
+    protected function readDayTypeAssignment(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
+    {
+        $id = (string) $xml['id'];
+        $this->dayTypeAssignments[$id]['order'] = (int) $xml['order'];
+        $this->dayTypeAssignments[$id]['isAvailable'] = ((string) $xml->isAvailable) !== 'false';
+        $this->dayTypeAssignments[$id]['OperatingPeriodRef'] = isset($xml->OperatingPeriodRef)
+            ? (string) $xml->OperatingPeriodRef['ref']
+            : null;
+        $this->dayTypeAssignments[$id]['DayTypeRef'] = (string) $xml->DayTypeRef['ref'];
+        $this->dayTypeAssignments[$id]['Date'] = (string) $xml->Date;
+    }
+
     protected function read(ChristmasTreeParser $reader, SimpleXMLElement $xml): void
     {
         $this->dumpers['']->addRecord([
             'id' => $xml['id'],
         ]);
+    }
+
+    protected function writeCalendar(): void
+    {
+        $unrolled = [];
+        foreach ($this->dayTypeAssignments as $dayTypeId => $assignment) {
+            $dayTypeRef = $assignment['DayTypeRef'];
+            if (!empty($assignment['Date'])) {
+                $unrolled[$assignment['Date']][$dayTypeRef] = $assignment['isAvailable'];
+                continue;
+            }
+            if (!empty($assignment['OperatingPeriodRef'])) {
+                foreach (
+                    $this->unrollDates(
+                        $this->operatingPeriods[$assignment['OperatingPeriodRef']],
+                        $this->dayTypes[$dayTypeRef]
+                    ) as $date
+                ) {
+                        $unrolled[$date][$dayTypeRef] = $assignment['isAvailable'];
+                }
+            }
+        }
+        foreach ($unrolled as $date => $dayTypes) {
+            foreach ($dayTypes as $dayTypeRef => $isAvail) {
+                if ($isAvail) {
+                    $this->dumpers['Calendar']->addRecord([
+                        'id' => "$date-$dayTypeRef",
+                        'date' => $date,
+                        'ref' => $dayTypeRef,
+                    ]);
+                }
+            }
+        }
+    }
+
+    protected function unrollDates($period, $dayType): array
+    {
+        $dates = [];
+        $fromDate = new Carbon($period['FromDate']);
+        $toDate = new Carbon($period['ToDate']);
+        $daysOfWeek = $this->daysToDow($dayType['DaysOfWeek']);
+        Log::debug(sprintf("%s => %s", $dayType['DaysOfWeek'], implode(', ', $daysOfWeek)));
+        while ($fromDate->lessThanOrEqualTo($toDate)) {
+            if (!empty($daysOfWeek[$fromDate->dayOfWeekIso])) {
+                $dates[] = $fromDate->toDateString();
+            }
+            $fromDate->addDay();
+        }
+        return $dates;
+    }
+
+    protected function daysToDow(string $days): array
+    {
+        if (strlen($days) < 5) {
+            return [];
+        }
+        $days = str_replace('Everyday', 'Weekdays Weekend', $days);
+        $days = str_replace('Weekdays', 'Monday Tuesday Wednesday Thursday Friday', $days);
+        $days = str_replace('Weekend', 'Saturday Sunday', $days);
+        return array_intersect([
+            1 => 'Monday',
+            2 => 'Tuesday',
+            3 => 'Wednesday',
+            4 => 'Thursday',
+            5 => 'Friday',
+            6 => 'Saturday',
+            7 => 'Sunday',
+        ], explode(' ', $days));
     }
 }
