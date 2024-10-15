df10d3f8eac7ecd77d16632889a1c5ba175def13
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2024-10-15 09:08:08 +0200

    fix: ServiceJourney > DayTypeRef can be multivalue

---
+++ database/migrations/2024_10_11_140000_netex_multi_daytype.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('netex_journey_day_types', function (Blueprint $table) {
+            $table->id()->comment('Laravel internal ID');
+            $table->char('service_journey_id')->index()->comment('References netex_vehicle_journey.id');
+            $table->char('day_type_ref')->comment('Daytype reference. This can be joined with `netex_calendar.ref` to retrieve dates.');
+        });
+
+        Schema::table('netex_vehicle_journeys', function (Blueprint $table) {
+            $table->dropColumn('calendar_ref');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('netex_journey_day_types');
+
+        Schema::table('netex_vehicle_journeys', function (Blueprint $table) {
+            $table->char('calendar_ref');
+        });
+    }
+};
